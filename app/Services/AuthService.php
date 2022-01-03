ffd9097d6af65e2a4a6d3938ec58b14fd9b60bfb
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-03 20:57:46 +0700

    feat: create AuthService with method currentRole()

---
+++ app/Services/AuthService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Auth;
+
+class AuthService {
+  public function currentRole() {
+    $roles = ['admin', 'lecturer', 'student'];
+
+    foreach ($roles as $role) {
+      if (Auth::guard($role)->check()) {
+        return $role;
+      }
+
+      return '';
+    };
+  }
+}
