98c64feb1d313540cc906e334321f6c4140fe3a9
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-03 21:19:44 +0700

    feat: make Lecturer to authenticatable

--- app/Models/Lecturer.php
+++ app/Models/Lecturer.php
@@ -3,15 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Lecturer extends Model
+class Lecturer extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens, Notifiable;
 
     protected $table = 'lecturers';
 
     protected $primaryKey = 'lecturer_id';
 
     protected $keyType = 'string';
+
+    protected $hidden = [
+        'password',
+    ];
 }
