ed2cb3303d8bd88613c72a6dce136ad53e3271ac
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-03 20:15:34 +0700

    refactor(web.php): use nested prefix route

--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,27 @@ use App\Http\Controllers\HomeController;
 */
 Route::get('/', [HomeController::class, 'get'])->middleware('guest');
 
-Route::get('/admin/dashboard', [Admin\DashboardController::class, 'get'])
-    ->name('admin-dashboard')
-    ->middleware('auth:admin');
+Route::prefix('admin')->group(function() {
+    Route::get('/login', [Admin\AuthController::class, 'get'])->middleware('guest:admin');
 
-Route::get('/admin/login', [Admin\AuthController::class, 'get'])->middleware('guest:admin');
+    Route::middleware('auth:admin')->group(function() {
+        Route::get('/dashboard', [Admin\DashboardController::class, 'get'])->name('admin-dashboard');
+    
+        Route::prefix('lecturers')->group(function() {
+            Route::get('/', [Admin\LecturerController::class, 'get']);
+            Route::get('/add', [Admin\LecturerController::class, 'add']);
+            Route::post('/add', [Admin\LecturerController::class, 'store']);
+            Route::delete('/delete/{lecturerId}', [Admin\LecturerController::class, 'delete']);
+            Route::get('/update/{lecturerId}', [Admin\LecturerController::class, 'edit']);
+            Route::put('/update/{lecturerId}', [Admin\LecturerController::class, 'update']);
+        });
+    });
+});
 
-Route::get('/admin/lecturers', [Admin\LecturerController::class, 'get'])->middleware('auth:admin');
 
-Route::get('/admin/lecturers/add', [Admin\LecturerController::class, 'add'])->middleware('auth:admin');
 
-Route::post('/admin/lecturers/add', [Admin\LecturerController::class, 'store'])->middleware('auth:admin');
 
-Route::delete('/admin/lecturers/delete/{lecturerId}', [Admin\LecturerController::class, 'delete'])->middleware('auth:admin');
 
-Route::get('/admin/lecturers/update/{lecturerId}', [Admin\LecturerController::class, 'edit'])->middleware('auth:admin');
-
-Route::put('/admin/lecturers/update/{lecturerId}', [Admin\LecturerController::class, 'update'])->middleware('auth:admin');
 
 Route::delete('/admin/logout', [Admin\AuthController::class, 'logout'])->middleware('auth:admin');
 
