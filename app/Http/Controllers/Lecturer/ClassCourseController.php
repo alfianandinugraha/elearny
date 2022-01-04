80a5ca87ceea314141e2dbebf6db7925d82d6627
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-04 10:29:33 +0700

    feat: show all join data to class page

--- app/Http/Controllers/Lecturer/ClassCourseController.php
+++ app/Http/Controllers/Lecturer/ClassCourseController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers\Lecturer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Lecturer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class ClassCourseController extends Controller
 {
     public function get() {
-        return view('pages.lecturer.classes.main');
+        $lecturerId = Auth::guard('lecturer')->id();
+
+        $courses = DB::table('lecturers')
+            ->where('lecturers.lecturer_id', $lecturerId)
+            ->join('class_courses', 'lecturers.lecturer_id', '=', 'class_courses.lecturer_id')
+            ->join('courses', 'courses.course_id', '=', 'class_courses.course_id')
+            ->select([
+                'class_courses.*', 'courses.name', 'courses.code', 'courses.semester'
+            ])
+            ->get();
+
+        return view('pages.lecturer.classes.main', [
+            'courses' => $courses
+        ]);
     }
 }
--- resources/views/pages/lecturer/classes/main.blade.php
+++ resources/views/pages/lecturer/classes/main.blade.php
@@ -15,6 +15,52 @@
                     >Tambah Kelas</a>
                 </div>
                 <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Kode</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Kelas</th>
+                                <th scope="col">Semester</th>
+                                <th scope="col">Token</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($courses as $course)
+                            <tr>
+                                <th>{{$course->code}}</th>
+                                <td>{{$course->name}}</td>
+                                <td>{{$course->class}}</td>
+                                <td>{{$course->semester}}</td>
+                                <td>
+                                    {!! $course->token ? 
+                                        $course->token : '<span class="text-danger">Belum ada</span>'
+                                    !!}
+                                </td>
+                                <td class="d-flex">
+                                    <a 
+                                        href="./classes/{{$course->class_course_id}}"  
+                                        class="btn btn-outline-primary mr-2"
+                                    >
+                                        <i class="fas fa-external-link-alt"></i>
+                                    </a>
+                                    <a 
+                                        class="btn btn-outline-primary mr-2"
+                                        href="./classes/{{$course->class_course_id}}/update"
+                                    >Update</a>
+                                    <form 
+                                        action="./classes/{{$course->class_course_id}}/delete" method="POST"
+                                    >
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-outline-danger">Hapus</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
