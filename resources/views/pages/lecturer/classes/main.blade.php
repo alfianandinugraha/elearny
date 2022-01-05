f4053c1a309db3b12fff63e0d2dcdf9779b02318
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-05 07:55:52 +0700

    refactor(lecturer/classes/main.blade.php): remove add class button

--- resources/views/pages/lecturer/classes/main.blade.php
+++ resources/views/pages/lecturer/classes/main.blade.php
@@ -7,13 +7,6 @@
     <div class="row">
         <div class="col-12">
             <div class="card shadow mb-4">
-                <div
-                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <a 
-                        class="mr-auto btn btn-primary"
-                        href="./classes/add"
-                    >Tambah Kelas</a>
-                </div>
                 <div class="card-body">
                     <table class="table">
                         <thead>
