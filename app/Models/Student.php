215013fd862fae842dcb25a519dab80c64f47dc1
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-05 08:12:39 +0700

    feat: make Student model as Authenticatable

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Student extends Model
+class Student extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens, Notifiable;
 
     protected $table = 'student';
 
