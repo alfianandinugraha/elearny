dc301d6d7935ca176064b8ded028c6f29f8bc573
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-08 13:34:37 +0700

    feat: show classes and materials on dashboard for student

--- app/Http/Controllers/Student/DashboardController.php
+++ app/Http/Controllers/Student/DashboardController.php
@@ -3,11 +3,43 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Models\Material;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function get() {
-        return view('pages.student.dashboard');
+        $studentId = Auth::guard('student')->id();
+
+        $studentClassQuery = DB::table('student_courses')
+            ->where('student_courses.student_id', $studentId)
+            ->join('class_courses', 'class_courses.class_course_id', '=', 'student_courses.class_course_id')
+            ->join('courses', 'courses.course_id', '=', 'class_courses.course_id');
+        $classes = $studentClassQuery->clone()
+            ->get(['courses.semester', 'courses.name AS course_name', 'class_courses.class', 'class_courses.class_course_id'])
+            ->toArray();
+        $totalClass = $studentClassQuery->clone()->count();
+
+        $materialsQuery = DB::table('student_courses')
+            ->where('student_courses.student_id', $studentId)
+            ->join('class_courses', 'class_courses.class_course_id', '=', 'student_courses.class_course_id')
+            ->join('courses', 'courses.course_id', '=', 'class_courses.course_id')
+            ->join('materials', 'materials.class_course_id', '=', 'class_courses.class_course_id');
+
+        $materials = $materialsQuery->clone()
+            ->limit(5)
+            ->get(['materials.*', 'courses.code'])
+            ->toArray();
+        $totalMaterial = $materialsQuery->clone()->count();
+
+        return view('pages.student.dashboard', [
+            'totalClass' => $totalClass,
+            'classes' => $classes,
+            'totalClass' => $totalClass,
+            'materials' => $materials,
+            'totalMaterial' => $totalMaterial
+        ]);
     }
 }
--- resources/views/pages/student/dashboard.blade.php
+++ resources/views/pages/student/dashboard.blade.php
@@ -6,14 +6,14 @@ Selamat Datang, <b>{{Auth::guard('student')->user()->fullname}}</b> !
 
 @section('content')
     <div class="row">
-        <div class="col-xl-4 col-md-6 mb-4">
+        <div class="col-xl-6 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Total Mata Kuliah</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">10</div>
+                                Total Kelas</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$totalClass}}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-chalkboard fa-2x text-gray-300"></i>
@@ -22,38 +22,16 @@ Selamat Datang, <b>{{Auth::guard('student')->user()->fullname}}</b> !
                 </div>
             </div>
         </div>
-        <div class="col-xl-4 col-md-6 mb-4">
+        <div class="col-xl-6 col-md-6 mb-4">
             <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Tugas</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">7</div>
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Materi</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$totalMaterial}}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-scroll fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-4 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Total SKS
-                            </div>
-                            <div class="row no-gutters align-items-center">
-                                <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                                    22
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-tasks fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
@@ -73,31 +51,33 @@ Selamat Datang, <b>{{Auth::guard('student')->user()->fullname}}</b> !
                         <thead>
                             <tr>
                                 <th scope="col">Mata Kuliah</th>
+                                <th scope="col">Kelas</th>
                                 <th scope="col">Semester</th>
                                 <th scope="col">Action</th>
                             </tr>
                         </thead>
                         <tbody>
+                            @foreach($classes as $class)
                             <tr>
-                                <th scope="row">Pemrograman Web</th>
-                                <td scope="row">5</td>
+                                <th scope="row">{{$class->course_name}}</th>
+                                <td scope="row">{{$class->class}}</td>
+                                <td scope="row">{{$class->semester}}</td>
                                 <td scope="row">
-                                    <button class="btn btn-outline-primary">
+                                    <a 
+                                        href="/student/classes/{{$class->class_course_id}}"
+                                        class="btn btn-outline-primary"
+                                    >
                                         <i class="fas fa-external-link-alt"></i>
-                                    </button>
+                                    </a>
                                 </td>
                             </tr>
-                            <tr>
-                                <th scope="row">Penelitan Informatika</th>
-                                <td scope="row">5</td>
-                                <td scope="row">
-                                    <button class="btn btn-outline-primary">
-                                        <i class="fas fa-external-link-alt"></i>
-                                    </button>
-                                </td>
-                            </tr>
+                            @endforeach
                         </tbody>
                     </table>
+                    <a href="/student/classes" class="btn btn-outline-primary w-100">
+                        <i class="fas fa-sm fa-external-link-alt mr-1"></i>
+                        Lihat Semua
+                    </a>
                 </div>
             </div>
         </div>
@@ -105,35 +85,38 @@ Selamat Datang, <b>{{Auth::guard('student')->user()->fullname}}</b> !
             <div class="card shadow mb-4">
                 <div
                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Tugas Aktif</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">Materi</h6>
                 </div>
                 <div class="card-body">
                     <table class="table">
                         <thead>
                             <tr>
-                                <th scope="col">Nama</th>
+                                <th scope="col">Judul</th>
+                                <th scope="col">Kode Mata Kuliah</th>
                                 <th scope="col">Action</th>
                             </tr>
                         </thead>
                         <tbody>
+                            @foreach($materials as $material)
                             <tr>
-                                <th scope="row">Membuat Laporan</th>
+                                <th scope="row">{{$material->title}}</th>
+                                <td scope="row">{{$material->code}}</td>
                                 <td scope="row">
-                                    <button class="btn btn-outline-primary">
+                                    <a 
+                                        href="/student/materials/{{$material->material_id}}"
+                                        class="btn btn-outline-primary"
+                                    >
                                         <i class="fas fa-external-link-alt"></i>
-                                    </button>
+                                    </a>
                                 </td>
                             </tr>
-                            <tr>
-                                <th scope="row">Kuis Pemrograman Web</th>
-                                <td scope="row">
-                                    <button class="btn btn-outline-primary">
-                                        <i class="fas fa-external-link-alt"></i>
-                                    </button>
-                                </td>
-                            </tr>
+                            @endforeach
                         </tbody>
                     </table>
+                    <a href="/student/materials" class="btn btn-outline-primary w-100">
+                        <i class="fas fa-sm fa-external-link-alt mr-1"></i>
+                        Lihat Semua
+                    </a>
                 </div>
             </div>
         </div>
