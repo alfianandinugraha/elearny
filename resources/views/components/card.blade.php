e45c655d749de9f52140f40f4dc1aa857f0ae8d6
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-09 02:01:42 +0700

    fix(components/card.blade.php): check if slot header is empty

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -4,18 +4,24 @@
 ])
 
 <?php
-    $headerAttributes = $header->attributes->merge([
-        'class' => 'card-header py-3 d-flex flex-row align-items-center'
-    ]);
+    $headerAttributes;
+
+    if (!empty($header)) {
+        $headerAttributes = $header->attributes->merge([
+            'class' => 'card-header py-3 d-flex flex-row align-items-center'
+        ]);
+    }
     $bodyAttributes = $body->attributes->merge([
         'class' => 'card-body'
     ]);
 ?>
 
 <div class="card shadow mb-4">
+    @if(!empty($header))
     <div {{$headerAttributes}}>
         {{$header}}
     </div>
+    @endif
     <div {{$bodyAttributes}}>
         {{$body}}
     </div>
